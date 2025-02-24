843bce0568f8f9655eaccf879d2267443a8e7824
Woody <sky.bome@gmail.com>
2025-02-24 16:37:29 +1100

    converting to LF

--- main.ts
+++ main.ts
@@ -1,11 +1,37 @@
 import { SetAssociativeCache } from "./src/SetAssociativeCache";
 
-
-
 // Create a cache with 4 sets and 2-way associativity
-const cache = new SetAssociativeCache<number, string>(4, 4);
+const cache = new SetAssociativeCache<number, string>(4, 2);
 
 // Insert some values
 cache.put(1, "One");
 cache.put(2, "Two");
+cache.put(3, "Three");
+cache.put(4, "Four");
+cache.put(5, "Five");
+cache.put(6, "Six");
+cache.put(7, "Seven");
+cache.put(8, "Eight");
+cache.put(9, "Nine");
+cache.put(10, "Ten");
 
+
+console.log("show all", cache.listAll());
+
+
+/*
+
+console.log("get key 1:", cache.get(1)); 
+console.log("get key 2:", cache.get(2)); 
+
+cache.put(1, "Another One");
+console.log("get another key 1:", cache.get(1)); 
+
+
+console.log("show all", cache.listAll());
+
+cache.delete(2);
+console.log("get key 2:", cache.get(2)); 
+
+console.log("show all", cache.listAll());
+*/
--- src/SetAssociativeCache.ts
+++ src/SetAssociativeCache.ts
@@ -13,12 +13,12 @@ export class SetAssociativeCache<Key extends string | number, Value> {
 
     // hash function to identify a specific set 
     private getSetIndex(key: Key): number {
-        const x = 1.618;        
+        const x = 1.618; 
         return (Number(key) * x | 0) % this.numberOfSets;           // even distribution with integer index
     }
 
     // put value with specific key
-    put(key: Key, value: Value | null) {        
+    put(key: Key, value: Value | null) {
         this.sets[this.getSetIndex(key)].store(key, value);
     }
 
